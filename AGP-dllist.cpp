92669fef5a57e73947ac9b99a3645454e2000ba2
H1nkk <the_h1nk@mail.ru>
2025-11-04 00:16:28 +0300

    add func slower

--- AGP-dllist.cpp
+++ AGP-dllist.cpp
@@ -10,10 +10,10 @@
 using namespace std;
 
 double r = 2.0; // method parameter
-const double E = 1e-4; // epsilon
-const double a = 2, b = 7; // left and right bounds
+const double E = 1e-3; // epsilon
 const int ITERMAX = 500;
-const int TIMEMEASUREITERS = 100;
+const int TIMEMEASUREITERS = 10;
+const int SLOWINGITERS = 10000;
 
 map<double (*)(double), double> extremums;
 map<double (*)(double), double> leftBound;
@@ -27,100 +27,101 @@ struct info {
 	info(double extremumArg, double  extremumVal, int iterCount) : extremumArg(extremumArg), extremumVal(extremumVal), iterCount(iterCount) {}
 };
 
-double f1(double x) {
-	if (x < (a + b) / 2) return 7 - x;
-	double intersec = 7 - (a + b) / 2;
-	return intersec - (a + b) / 2 + x;
+double funcSlower(double x) {
+	double k = 1;
+	for (int i = 0; i < SLOWINGITERS; i++) {
+		k *= (cos(x) * cos(x) + sin(x) * sin(x));
+	}
+	return k;
 }
-
-double f2(double x) {
-	return x * sin(x);
-}
-
-double f3(double x) {
-	return x * cos(x);
-}
-
-double f4(double x) {
-	return 354318.0 * x * cos(x);
-}
-
-double f5(double x) {
-	return 3 * x * cos(4 * x);
-}
-
-double f6(double x) {
-	return x * cos(x * x);
-}
-
-double f7(double x) {
-	return -x - a;
-}
-
 
 double becnhFunc1(double x) {
-	return sin(x) + sin(10.0 * x / 3.0);
+	double k = funcSlower(x);
+	double res = sin(x) + sin(10.0 * x / 3.0);
+
+	res *= k;
+	return res;
 }
 
 double becnhFunc2(double x) {
+	double k = funcSlower(x);
 	double res = 0;
-	for (int k = 1; k <= 5; k++) {
+	for (double k = 1.0; k <= 5.0; k += 1.0) {
 		res += k * sin((k + 1) * x + k);
 	}
-	return -res;
+	res *= -1;
+
+	res *= k;
+	return res;
 }
 
 double becnhFunc3(double x) {
-	return (3.0 * x - 1.4) * sin(18.0 * x);
+	double k = funcSlower(x);
+	double res = (3.0 * x - 1.4) * sin(18.0 * x);
+
+	res *= k;
+	return res;
 }
 
 double becnhFunc4(double x) {
+	double k = funcSlower(x);
 	double res = -(x + sin(x));
 	res *= exp(-(x * x));
+
+	res *= k;
 	return res;
 }
 
 double becnhFunc5(double x) {
+	double k = funcSlower(x);
 	double res = sin(x) + sin(10.0 * x / 3.0) + log(x) - 0.84 * x + 3.0;
+
+	res *= k;
 	return res;
 }
 
 double becnhFunc6(double x) {
+	double k = funcSlower(x);
 	double res = sin(x) + sin(10.0 * x / 3.0) + log(x) - 0.84 * x + 3.0;
+
+	res *= k;
 	return res;
 }
 
 double becnhFunc7(double x) {
+	double k = funcSlower(x);
 	double res = -sin(2 * numbers::pi_v<double> *x) * exp(-x);
+
+	res *= k;
 	return res;
 }
 
 double becnhFunc8(double x) {
+	double k = funcSlower(x);
 	double res = (x * x - 5.0 * x + 6.0);
 	res /= (x * x + 1.0);
+
+	res *= k;
 	return res;
 }
 
 double becnhFunc9(double x) {
-	double res = -x + sin(3.0 * x) - 1;
+	double k = funcSlower(x);
+	double res = -x + sin(3.0 * x) - 1.0;
+
+	res *= k;
 	return res;
 }
 
 double becnhFunc10(double x) {
-	double res = 2 * (x - 3.0) * (x - 3.0) + exp(x * x * 0.5);
+	double k = funcSlower(x);
+	double res = 2.0 * (x - 3.0) * (x - 3.0) + exp(x * x * 0.5);
+
+	res *= k;
 	return res;
 }
 
 void initMaps() {
-	extremums[f1] = (a + b) / 2;
-
-	// для a = 2, b = 7:
-	extremums[f2] = 4.91318;
-	extremums[f3] = 3.42562;
-	extremums[f4] = 3.42562;
-	extremums[f5] = 7;
-	extremums[f6] = 6.86546;
-
 	extremums[becnhFunc1] = 5.145735;
 	leftBound[becnhFunc1] = 2.7;
 	rightBound[becnhFunc1] = 7.5;
@@ -279,73 +280,34 @@ info AGP(double a, double b, double (*func)(double x)) {
 	return res;
 }
 
-double findBestR(double (*testingFuncion)(double)) {
-	cout << fixed;
-
-	double extrPoint = extremums[testingFuncion];
-
-	double best = r, diff = fabs(AGP(a, b, testingFuncion).extremumArg - extrPoint);
-	for (r = 1.1; r < 100.0; r += 0.1) {
-		double curdiff = fabs(AGP(a, b, testingFuncion).extremumArg - extrPoint);
-		if (curdiff <= diff) {
-			best = r;
-			diff = curdiff;
-			cout << best << ' ' << diff << '\n';
-		}
-	}
-
-	cout << best << ' ' << diff << '\n';
-	return best;
-}
-
-
-void timeMeasure() {
-	r = 2.0;
-	info res{ 0,0,0 };
-	double avgTimeSpent = 0;
-	double foo = 4;
-	for (int i = 0; i < TIMEMEASUREITERS; i++) { // усредняем время, т.к. оно колеблется - думаю, что из-за частого выделения памяти
-		double (*testingFunction)(double) = funcs[i % 10];
-
-		auto start = chrono::high_resolution_clock::now();
-		res = AGP(a, b, testingFunction);
-		auto stop = chrono::high_resolution_clock::now();
-		auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-		double timeSpent = duration.count() / 1000000.0;
-		avgTimeSpent += timeSpent;
-		foo += res.extremumArg;
-	}
-	avgTimeSpent /= TIMEMEASUREITERS;
-
-	cout << "\n##########################\n";
-	cout << "Extremum argument: " << res.extremumArg << '\n';
-	cout << "Iteration count: " << res.iterCount << '\n';
-	cout << "Extremum value: " << res.extremumVal << '\n';
-	cout << "Calculated in " << avgTimeSpent << " seconds\n";
-	cout << "\nfoo for release:" << foo << "\n##########################\n";
-}
-
-void benchTests() {
+void benchTimeTests() {
 	for (int i = funcs.size() - 1; i >= 0; i--) {
-		info res = AGP(leftBound[funcs[i]], rightBound[funcs[i]], funcs[i]);
-		if (res.extremumArg != extremums[funcs[i]]) {
-			cout << "Func " << i + 1 << ". AGP result: " << res.extremumArg << ", actual result: " << extremums[funcs[i]] << '\n';
-			cout << "Difference in results: " << fabs(res.extremumArg - extremums[funcs[i]]);
-			cout << "\nIterations count : " << res.iterCount << "\n\n";
-			cout << flush;
+		double (*testingFunction)(double) = funcs[i];
 
+		info res(0, 0, 0);
+		double minTimeSpent = INFINITY;
+		for (int i = 0; i < TIMEMEASUREITERS; i++) {
+			auto start = chrono::high_resolution_clock::now();
+			res = AGP(leftBound[testingFunction], rightBound[testingFunction], testingFunction);
+			auto stop = chrono::high_resolution_clock::now();
+			auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+			double timeSpent = duration.count() / 1000000.0;
+			minTimeSpent = min(minTimeSpent, timeSpent);
 		}
+		cout << "Func " << i + 1 << ". AGP result: " << res.extremumArg << ", actual result: " << extremums[funcs[i]] << '\n';
+		cout << "Difference in results: " << fabs(res.extremumArg - extremums[funcs[i]]);
+		cout << "\nIterations count : " << res.iterCount << "\n";
+		cout << "Minimum calculating time : " << minTimeSpent << "\n";
+		cout << '\n';
+		cout << flush;
 	}
 }
 
-int main() { // (или нет) реализация на dllist эффективнее при ITERCOUNT <= ~5000, потом время ухудшается из-за выделения и очищения памяти в куче
+int main() {
 	initMaps();
 
 	cout << fixed;
 
-	benchTests();
-
-	timeMeasure(); // первый запуск работает в ~1.5 раза медленнее. прогрев? #################################################################
-
+	benchTimeTests();
 	return 0;
 }
